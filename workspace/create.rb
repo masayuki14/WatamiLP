5405514c50a4d4c40cb4ce24b7f62f1d17a18741
Masayuki Morisaki <masayuki144@gmail.com>
2016-03-20 22:18:27 +0900

    image,css,js のディレクトリをコピー

--- workspace/create.rb
+++ workspace/create.rb
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 require 'yaml'
 require 'csv'
+require 'fileutils'
 
 ROOT_DIR  = File.dirname(File.expand_path(__FILE__))
 config = YAML.load_file(ROOT_DIR + '/config.yml')
@@ -14,6 +15,11 @@ rows = Watami::Row.parse(source_data)
 # 出力先のディレクトリを作成
 output_dir = File.join(ROOT_DIR, config['output_dir'])
 Dir.mkdir(output_dir) unless Dir.exists?(output_dir)
+
+# img, css, js ディレクトリをコピー
+FileUtils.cp_r(File.join(ROOT_DIR, config['source_image_dir']), output_dir)
+FileUtils.cp_r(File.join(ROOT_DIR, config['source_css_dir']), output_dir)
+FileUtils.cp_r(File.join(ROOT_DIR, config['source_javascript_dir']), output_dir)
 
 # erb にデータをバインドして出力
 rows.each { |row| row.write_template(ROOT_DIR, config) }
