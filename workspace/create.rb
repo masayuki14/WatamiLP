d847cec0f5751570955f4c00b8d9489fc8dc1d23
Masayuki Morisaki <masayuki144@gmail.com>
2016-03-25 11:31:26 +0900

    assetsのコピーモードに対応

--- workspace/create.rb
+++ workspace/create.rb
@@ -15,23 +15,35 @@ rows = Watami::Row.parse(source_data)
 
 # 出力先のディレクトリを作成
 output_dir = File.join(ROOT_DIR, config['output_dir'])
-Dir.mkdir(output_dir) unless Dir.exists?(output_dir)
-
-# img, css, js ディレクトリをコピー
-
-FileUtils.cp_r(File.join(ROOT_DIR, config['source_image_dir']), output_dir)
-FileUtils.cp_r(File.join(ROOT_DIR, config['source_css_dir']), output_dir)
-FileUtils.cp_r(File.join(ROOT_DIR, config['source_javascript_dir']), output_dir)
+FileUtils.mkdir_p(output_dir)
 
 # erb にデータをバインドして出力
 erb_file = File.join(ROOT_DIR, config['source_index_file'])
+output_file_dir = ''
 rows.each do |row|
   row.bind_erb(erb_file) do |binded|
     # 出力ファイル名とディレクトリの作成
-    output_file     = File.join(ROOT_DIR, config['output_dir'], row.replace_by_attr(config['output_file_name']))
+    output_file     = File.join(output_dir, row.replace_by_attr(config['output_file_name']))
     output_file_dir = File.dirname(File.expand_path(output_file))
     FileUtils.mkdir_p(output_file_dir)
 
+    # assetsをコピー
+    if config['assets_copy_mode'] == 'separate' ||
+        (config['assets_copy_mode'] == 'auto' && output_dir != output_file_dir)
+
+      FileUtils.cp_r(File.join(ROOT_DIR, config['source_image_dir']), output_file_dir)
+      FileUtils.cp_r(File.join(ROOT_DIR, config['source_css_dir']), output_file_dir)
+      FileUtils.cp_r(File.join(ROOT_DIR, config['source_javascript_dir']), output_file_dir)
+    end
+
     File.open(output_file, 'w') { |file| file.write(binded) }
   end
+end
+
+# img, css, js ディレクトリをコピー
+if config['assets_copy_mode'] == 'bulk' ||
+    (config['assets_copy_mode'] == 'auto' && output_dir == output_file_dir)
+  FileUtils.cp_r(File.join(ROOT_DIR, config['source_image_dir']), output_dir)
+  FileUtils.cp_r(File.join(ROOT_DIR, config['source_css_dir']), output_dir)
+  FileUtils.cp_r(File.join(ROOT_DIR, config['source_javascript_dir']), output_dir)
 end
